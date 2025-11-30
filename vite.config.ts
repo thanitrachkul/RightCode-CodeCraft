b751e2e6f75373ce18515c346d6d51cbb229f193
thanitrachkul <shiribadtosai@gmail.com>
2025-11-30 21:14:54 +0700

    Add base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+        base: '/RightCode-CodeCraft/',
       server: {
         base: '/RightCode-CodeCraft/',
         port: 3000,
